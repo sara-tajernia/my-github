618bfa39615895d8f6e238cefca5495ad6d90e19
sara_tjn <tajernia.sara@gmail.com>
2023-01-06 14:31:09 +0330

    end of project

--- app.js
+++ app.js
@@ -1,71 +1,136 @@
-function not_found(){
-    document.getElementById('not-found').style.display='none'
-}
-
-function server_error(){
-    document.getElementById('server-error').style.display='none'
-}
-
-function local_storage1(){
-    console.log('sara')
-    document.getElementById('local-storage').style.display='none'
-}
-
-async function getinfo(e) {
+/**
+ * remove not found modal
+ */
+function not_found() {
+    document.getElementById("not-found").style.display = "none";
+  }
+  /**
+   * remove server error modal
+   */
+  function server_error() {
+    document.getElementById("server-error").style.display = "none";
+  }
+  /**
+   * remove localstorage modal
+   */
+  function local_storage1() {
+    console.log("sara");
+    document.getElementById("local-storage").style.display = "none";
+  }
+  /**
+   * @param  {string} method
+   * @param  {string} url
+   * send XMLHTTP request to input url and return response promise
+   */
+  const makeRequest = (method, url) => {
+    // define new xml request
+    const xhr = new XMLHttpRequest();
+    return new Promise((resolve) => {
+      xhr.open(method, url, true);
+      // onload function when request done
+      xhr.onload = () =>
+        resolve({
+          status: xhr.status,
+          response: xhr.responseText,
+        });
+      //   handle errors
+      xhr.onerror = () => {
+        resolve({
+          status: xhr.status,
+          response: xhr.responseText,
+        });
+        server_error = document.getElementById("server-error").style.display =
+          "block";
+      };
+      xhr.send();
+    });
+  };
+  /**
+   * @param  {Event} e submit event
+   * send request to get user datas and find most lang in 5 last pushed repos
+   */
+  async function getinfo(e) {
     e.preventDefault();
-    var request = new XMLHttpRequest()    
-    url = 'https://api.github.com/users/'
-    const username = document.getElementById("user-id").value
+  
+    url = "https://api.github.com/users/";
+    const username = document.getElementById("user-id").value;
+  //   check for localstorage
     if (username in localStorage) {
-        var data = localStorage.getItem(document.getElementById("user-id").value)
-        data=JSON.parse(data)
-        console.log(data)
-        document.getElementById("avatar-url").src = data.avatar_url
-        document.getElementById("name").innerHTML = data.name
-        document.getElementById("blog").setAttribute('href', data.blog)
-        document.getElementById("location").innerHTML = data.location
-        document.getElementById("bio").innerHTML = data.bio
-        document.getElementById("location").innerHTML = data.location
-        server_error = document.getElementById('local-storage').style.display='block'
-    }else{
-        request.open('GET', url + document.getElementById("user-id").value, true)
-        request.onload = function () {
-        var data = JSON.parse(this.response)
-            if (request.status >= 200 && request.status < 400) {
-                console.log('success', data)
-                document.getElementById("avatar-url").src = data.avatar_url
-                document.getElementById("name").innerHTML = data.name
-                document.getElementById("blog").setAttribute('href', data.blog)
-                document.getElementById("location").innerHTML = data.location
-                document.getElementById("bio").innerHTML = data.bio
-                localStorage.setItem(document.getElementById("user-id").value, this.response);
-            }
+      var data = localStorage.getItem(username);
+      data = JSON.parse(data);
+      console.log(data);
+      document.getElementById("avatar-url").src = data.avatar_url;
+      document.getElementById("name").innerHTML = data.name;
+      document.getElementById("blog").setAttribute("href", data.blog);
+      document.getElementById("location").innerHTML = data.location;
+      document.getElementById("bio").innerHTML = data.bio;
+      document.getElementById("location").innerHTML = data.location;
+      document.getElementById("bio").innerHTML = data.bio;
+      document.getElementById("lang").innerHTML = 'Favorite language: ' + data.lang;
 
-            else {
-                console.log('error')
-                not_found = document.getElementById('not-found').style.display='block'
-                document.getElementById("avatar-url").src = "assets/profile.jpg"
-                document.getElementById("name").innerHTML = "Name"
-                document.getElementById("blog").innerHTML = "Address link"
-                document.getElementById("location").innerHTML = "Country/City"
-                document.getElementById("bio").innerHTML = "Bio"
-                // close = document.getElementById('signup').style.display='none'
-            }
+  
+      server_error = document.getElementById("local-storage").style.display =
+        "block";
+    } else {
+      // send request to get main data
+      const request = await makeRequest("GET", url + username);
+      if (request.status >= 200 && request.status <= 400) {
+        var data = JSON.parse(request.response);
+      // get user repos
+        const request_repo = await makeRequest(
+          "GET",
+          data.repos_url + "?per_page=5&sort=pushed"
+        );
+        var request_repo_res = JSON.parse(request_repo.response);
+        var languages = [];
+        // find 5 repos languages
+      //   get languages for user repos
+        for (let i = 0; i < request_repo_res.length; i++) {
+          var request_language = await makeRequest(
+            "GET",
+            request_repo_res[i].languages_url
+          );
+          languages.push(JSON.parse(request_language.response));
         }
-        request.onerror= function(e) {
-            server_error = document.getElementById('server-error').style.display='block'
-        };
-        request.send()
+        var max = 0;
+        var lang = "";
+        for (let i = 0; i < languages.length; i++) {
+          for (key in languages[i]) {
+            console.log(languages[i][key], key);
+            if (languages[i][key] > max) {
+              max = languages[i][key];
+              lang = key;
+            }
+          }
+        }
+        data["lang"] = lang;
+      //   change page content
+        document.getElementById("avatar-url").src = data.avatar_url;
+        document.getElementById("name").innerHTML = data.name;
+        document.getElementById("blog").setAttribute("href", data.blog);
+        document.getElementById("location").innerHTML = data.location;
+        document.getElementById("bio").innerHTML = data.bio;
+        document.getElementById("lang").innerHTML = 'Favorite language: ' + data.lang;
+
+      // set local storage
+        localStorage.setItem(username, JSON.stringify(data));
+      } else {
+        console.log("error");
+      //   change page content for error handling
+        not_found = document.getElementById("not-found").style.display = "block";
+        document.getElementById("avatar-url").src = "assets/profile.jpg";
+        document.getElementById("name").innerHTML = "Name";
+        document.getElementById("blog").innerHTML = "Address link";
+        document.getElementById("location").innerHTML = "Country/City";
+        document.getElementById("bio").innerHTML = "Bio";
+        // close = document.getElementById('signup').style.display='none'
+      }
     }
-}
-
-document.getElementById('submit-btn').addEventListener('submit', getinfo)
-document.getElementById('not-found').addEventListener('click', not_found)
-document.getElementById('server-error').addEventListener('click', server_error)
-document.getElementById('local-storage-btn').addEventListener('click', local_storage1)
-
-// GIT_REPO="https://api.github.com/repos/kubernetes/kubernetes" # Input Git Repo
-// BRANCH_NAME="master"                                          # Input Branch Name
-// COMMITS_NUM="5"                                               # Input to get last "N" number of commits
-
-// curl --silent --insecure --request GET --header "Accept: application/vnd.github.inertia-preview+json" "$GIT_REPO/commits?sha=$BRANCH_NAME&page=1&per_page=1000" | jq --raw-output '.[] | "\(.sha)|\(.commit.author.date)|\(.commit.message)|\(.commit.author.name)|\(.commit.author.email)" | gsub("[\n\t]"; "")' | awk 'NF' | awk '{$1=$1;print}' | head -$COMMITS_NUM
+  }
+  // add event listenrs
+  document.getElementById("submit-btn").addEventListener("submit", getinfo);
+  document.getElementById("not-found").addEventListener("click", not_found);
+  document.getElementById("server-error").addEventListener("click", server_error);
+  document
+    .getElementById("local-storage-btn")
+    .addEventListener("click", local_storage1);
